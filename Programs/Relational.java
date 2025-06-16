a7e699a9dcc3018ef3352d4407b72e0c5e6030ab
Chandran-D <chandrand2005@gmail.com>
2025-06-16 11:18:12 +0530

    Add files via upload

---
+++ Programs/Relational.java
@@ -0,0 +1,13 @@
+class Relational{
+      public static void main(String[]args){
+        char a = 'A';
+        char b = 'B';
+        System.out.println("A"+ "<"+"B"+"is"+(a<b) );
+        System.out.println("A"+ "<="+"B"+"is"+(a<=b) );
+        System.out.println("A"+ ">"+"B"+"is"+(a>b) );
+        System.out.println("A"+ ">="+"B"+"is"+(a>=b) );
+        System.out.println("A"+ "=="+"B"+"is"+(a==b) );
+        System.out.println("A"+ "!="+"B"+"is"+(a!=b) );
+      }
+
+}
